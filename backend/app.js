67053d99e44fffa814997841dbbd5dc78c86b45c
mooorc-code <62363881+mooorc-code@users.noreply.github.com>
2021-07-15 19:25:07 +0200

    mise en place authentification

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+//modules//
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
+const mongoose = require( 'mongoose' );
+const userRoutes = require( './routes/user' );
+const path = require('path');
+
+
+
+//MongoDb//
+mongoose.connect( 'mongodb+srv://admin:171060@cluster0.eoczz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    } )
+    .then( () => console.log( 'Connexion à MongoDB réussie !' ) )
+    .catch( () => console.log( 'Connexion à MongoDB échouée !' ) );
+
+const app = express();
+
+app.use( (req, res, next) => {
+    res.setHeader( 'Access-Control-Allow-Origin', '*' );
+    res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization' );
+    res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS' );
+    next();
+} );
+
+app.use( bodyParser.json() );
+//routers//
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use( '/api/auth', userRoutes);
+
+
+module.exports = app;
+
